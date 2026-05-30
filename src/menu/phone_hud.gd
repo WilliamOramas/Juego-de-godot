b786a648511e9b21c3bfea1e26714083e00c3fda
Enmanuel Antoni Bracho Villanueva <enmanuel.bracho@zuluapp.io>
2026-05-30 16:49:04 -0400

    feat: implement player character scene, add phone HUD UI, and configure Phone input mapping.

---
+++ src/menu/phone_hud.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var face_anim: AnimationPlayer = $Phone/FacePlayer
+
+var is_open: bool = false
+
+func _ready() -> void:
+	# Asegurarnos de que el teléfono empiece escondido y desactivado
+	$Phone.position = Vector2(580, 650)
+	visible = false
+	face_anim.play("talk")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Phone"):
+		toggle_phone()
+
+func toggle_phone() -> void:
+	if is_open:
+		close_phone()
+	else:
+		open_phone()
+
+func open_phone() -> void:
+	is_open = true
+	visible = true
+	anim.play("slide_in")
+
+func close_phone() -> void:
+	is_open = false
+	anim.play("slide_out")
+	await anim.animation_finished
+	if not is_open:
+		visible = false
